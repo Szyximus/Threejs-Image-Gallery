33e8cbcc8eeccab2e3e71c982d4d5980cb67b240
Szymon Jakóbczyk <sjakobczyk@gmail.com>
2020-01-18 18:30:08 +0100

    move from connect to server

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ fs.readdir(directoryPath, function (err, files) {
 
 });
 
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
+
 
 //var connect = require('connect');
 //var serveStatic = require('serve-static');
